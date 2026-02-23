1756dd291cb1a2c712429871f98eb2768c3ad7f5
Athul Krishna M A <66295880+athulkrishnama@users.noreply.github.com>
2026-02-23 05:25:30 +0000

    new yaml added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("running application");
-setInterval(()=>console.log("Application is running"), 1000)
-setTimeout(()=>{throw new Error("completed")}, 5000)
+const interval = setInterval(()=>console.log("Application is running"), 1000)
+setTimeout(()=>{clearInterval(interval)}, 5000)
 // sample change
