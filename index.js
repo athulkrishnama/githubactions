429378b0a0e934a96d3c881a97e1ac892fed3ba8
Athul Krishna M A <66295880+athulkrishnama@users.noreply.github.com>
2026-02-23 05:32:04 +0000

    asdf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("running application");
 const interval = setInterval(()=>console.log("Application is running"), 1000)
 setTimeout(()=>{clearInterval(interval)}, 5000)
-// sample change
+// sample change 
